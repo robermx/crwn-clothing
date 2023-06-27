f23c2e77ae76400d263c5b3fc859a4248971125e
Roberto Arenas <roberto.arenas2@icloud.com>
2023-06-27 16:44:38 -0600

    [Feat] Applying a data array to component. Medium.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jacket',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
   return (
-    <div>
-      <h1>Hi Rob!!</h1>
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+          <div className="category--container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
